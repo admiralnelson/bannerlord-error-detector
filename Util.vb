b20ffe40598f427a40dc00303a7f369659a85c1c
horatio nelson <admiralofinternetmeme@outlook.com>
2020-05-17 09:58:52 +0800

    + add Util.vb - Removed taleworlds dlls from the project compilation

---
+++ Util.vb
@@ -0,0 +1,26 @@
+﻿
+Imports System.IO
+Imports Newtonsoft.Json
+
+Module Util
+    Public AllowInDebugger As Boolean = True
+    Public CatchOnApplicationTick As Boolean = True
+    Public CatchOnMissionScreenTick As Boolean = True
+    Public CatchOnFrameTick As Boolean = True
+    Public CatchTick As Boolean = True
+    Public SaveLogPath As String = ""
+    Public Sub ReadConfig()
+        Try
+            Dim data = File.ReadAllText("..\..\Modules\BetterExceptionWindow\config.json")
+            Dim configData = JsonConvert.DeserializeObject(data)
+            AllowInDebugger = configData("AllowInDebugger")
+            CatchOnApplicationTick = configData("CatchOnApplicationTick")
+            CatchOnMissionScreenTick = configData("CatchOnMissionScreenTick")
+            CatchOnFrameTick = configData("CatchOnFrameTick")
+            CatchTick = configData("CatchTick")
+            SaveLogPath = configData("SaveLogPath")
+        Catch ex As Exception
+            Console.WriteLine("unable to read the config. It's ok")
+        End Try
+    End Sub
+End Module
