2910cadd9e7d986eb7a99f2c9fb18651942701a0
admiralnelson <admiralofinternetmeme@outlook.com>
2022-03-30 21:39:14 +0800

    forget to upload the UI stuff

---
+++ supporting modules/BetterExceptionWindowUI/BewConfigurationUserInterface.vb
@@ -0,0 +1,114 @@
+﻿Imports MCM.Abstractions.FluentBuilder
+Imports MCM.Abstractions.Ref
+Imports BetterExceptionWindow
+Public Module BewConfigurationUserInterface
+    Dim registered = False
+    Public Sub BuildUI()
+        If registered Then Exit Sub
+        BaseSettingsBuilder _
+        .Create(
+            "org.calradia.admiralnelson.betterexceptionwindow.ui.config",
+            "Better Exception Window") _
+        .SetFormat("json2") _
+        .CreateGroup(
+            "Behaviour",
+            Function(builder)
+                Return builder _
+                .AddBool(
+                    "AllowInDebugger",
+                    "{=AllowInDebugger}Allow In Debugger",
+                    New ProxyRef(Of Boolean)(
+                    Function() As Boolean
+                        Return AllowInDebugger
+                    End Function,
+                    Sub(o As Boolean)
+                        AllowInDebugger = o
+                        SaveSettings()
+                    End Sub),
+                    Function(boolBuilder)
+                        Return boolBuilder _
+                               .SetHintText("{=AllowInDebuggerHint}Better Exception Window will trigger when debugger is attached")
+                    End Function) _
+                 .AddButton(
+                 "Crashtest",
+                 "{=CrashTest}Crash Test: Throw an exception",
+                   New ProxyRef(Of Action)(
+                    Function() As Action
+                        Return Sub()
+                                   MsgBoxBannerlord("Warning!", "This will crash your game! Are you sure?",
+                                     Sub()
+                                         Throw New Exception("Exception test on UI event")
+                                     End Sub,
+                                     Sub()
+
+                                     End Sub,
+                                     "Do as I say!"
+                                   )
+                               End Sub
+                    End Function,
+                    Sub(o As Action)
+
+                    End Sub),
+                  "{=TestItNow}Test it now",
+                  Function(boolBuilder)
+                      Return boolBuilder _
+                            .SetHintText("{=AllowInDebuggerHint}This will crash your game!")
+                  End Function) _
+                 .AddButton(
+                 "Restart",
+                 "{=CrashTest}Restart with dnSpy attached",
+                   New ProxyRef(Of Action)(
+                    Function() As Action
+                        Return Sub()
+                                   If Debugger.IsAttached Then
+                                       Print("The game is running under debugger already")
+                                       Exit Sub
+                                   End If
+                                   If IsDnspyAvailable() Then
+                                       MsgBoxBannerlord("Warning!", "This will restart your game! Are you sure? Save your game first if neccessary",
+                                         Sub()
+                                             RestartAndAttachDnspy()
+                                         End Sub,
+                                         Sub()
+
+                                         End Sub,
+                                         "Do as I say!"
+                                       )
+                                       Exit Sub
+                                   End If
+                                   Print("Dnspy is not installed in BetterExceptionWindow")
+                               End Sub
+                    End Function,
+                    Sub(o As Action)
+
+                    End Sub),
+                  "{=TestItNow}Restart",
+                  Function(boolBuilder)
+                      Return boolBuilder _
+                            .SetHintText("{=AllowInDebuggerHint}Restart the game with debugger attached")
+                  End Function) _
+                 .AddButton(
+                 "About",
+                 "{=CrashTest}About BetterExceptionWindow",
+                   New ProxyRef(Of Action)(
+                    Function() As Action
+                        Return Sub()
+                                   MsgBoxBannerlord("About Better Exception Window",
+                                                     Version + vbNewLine +
+                                                    "Git commit: " + Commit)
+                               End Sub
+                    End Function,
+                    Sub(o As Action)
+
+                    End Sub),
+                  "{=TestItNow}About...",
+                  Function(boolBuilder)
+                      Return boolBuilder _
+                            .SetHintText("{=AllowInDebuggerHint}About this program.")
+                  End Function)
+            End Function) _
+            .BuildAsGlobal() _
+            .Register()
+        registered = True
+    End Sub
+End Module
---
+++ supporting modules/BetterExceptionWindowUI/EntryPoint.vb
@@ -0,0 +1,13 @@
+﻿Imports MCM.Abstractions
+Imports MCM.Abstractions.FluentBuilder
+Imports MCM.Abstractions.Ref
+Imports MCM.Abstractions.Settings.Base
+Imports BetterExceptionWindow
+
+Namespace Global.BetterExceptionWindowConfigUI
+    Public Module EntryPoint
+        Public Sub Start()
+            BuildUI()
+        End Sub
+    End Module
+End Namespace
