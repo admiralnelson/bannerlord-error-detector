baca015cebc242c0d9737a105523879ec82e3c23
admiralnelson <admiralofinternetmeme@outlook.com>
2022-10-15 16:52:41 +0700

    edited title window

--- ErrorWindow.Designer.vb
+++ ErrorWindow.Designer.vb
@@ -36,10 +36,10 @@ Partial Class ErrorWindow
         '
         Me.widget.Dock = System.Windows.Forms.DockStyle.Fill
         Me.widget.Location = New System.Drawing.Point(0, 0)
-        Me.widget.Margin = New System.Windows.Forms.Padding(10, 9, 10, 9)
-        Me.widget.MinimumSize = New System.Drawing.Size(63, 57)
+        Me.widget.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.widget.MinimumSize = New System.Drawing.Size(27, 25)
         Me.widget.Name = "widget"
-        Me.widget.Size = New System.Drawing.Size(3765, 1301)
+        Me.widget.Size = New System.Drawing.Size(810, 456)
         Me.widget.TabIndex = 3
         '
         'widgetMenu
@@ -47,37 +47,37 @@ Partial Class ErrorWindow
         Me.widgetMenu.ImageScalingSize = New System.Drawing.Size(48, 48)
         Me.widgetMenu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CopyToolStripMenuItem, Me.SelectAllToolStripMenuItem, Me.SaveToolStripMenuItem})
         Me.widgetMenu.Name = "widgetMenu"
-        Me.widgetMenu.Size = New System.Drawing.Size(247, 172)
+        Me.widgetMenu.Size = New System.Drawing.Size(141, 76)
         '
         'CopyToolStripMenuItem
         '
         Me.CopyToolStripMenuItem.Name = "CopyToolStripMenuItem"
-        Me.CopyToolStripMenuItem.Size = New System.Drawing.Size(246, 56)
+        Me.CopyToolStripMenuItem.Size = New System.Drawing.Size(140, 24)
         Me.CopyToolStripMenuItem.Text = "Copy"
         '
         'SelectAllToolStripMenuItem
         '
         Me.SelectAllToolStripMenuItem.Name = "SelectAllToolStripMenuItem"
-        Me.SelectAllToolStripMenuItem.Size = New System.Drawing.Size(246, 56)
+        Me.SelectAllToolStripMenuItem.Size = New System.Drawing.Size(140, 24)
         Me.SelectAllToolStripMenuItem.Text = "Select All"
         '
         'SaveToolStripMenuItem
         '
         Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
-        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(246, 56)
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(140, 24)
         Me.SaveToolStripMenuItem.Text = "Save"
         '
         'ErrorWindow
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(19.0!, 37.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(3765, 1301)
+        Me.ClientSize = New System.Drawing.Size(810, 456)
         Me.ControlBox = False
         Me.Controls.Add(Me.widget)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-        Me.Margin = New System.Windows.Forms.Padding(10, 9, 10, 9)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "ErrorWindow"
-        Me.Text = "ErrorWindow"
+        Me.Text = "Program encountered a problem"
         Me.widgetMenu.ResumeLayout(False)
         Me.ResumeLayout(False)
 
