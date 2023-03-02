fcc2ee7f29ee38b203ed00f32183d5ab2c46b787
admiralnelson <admiralofinternetmeme@outlook.com>
2023-03-02 12:35:57 +0700

    removed 1st prompt upgraded version number

--- Main.vb
+++ Main.vb
@@ -36,28 +36,6 @@ Namespace Global.BetterExceptionWindow
         Private Sub ControlPanelMcmSupport()
             Task.Delay(1000 * 2).ContinueWith(
                 Sub()
-                    If IsFirstTime Then
-                        Task.Delay(1000 * 3).ContinueWith(
-                        Sub()
-                            MsgBoxBannerlord("Native code to Managed code handler",
-                                            "From version 6.1, Better Exception Window now catches exception thrown " &
-                                            "from Native code to Managed code transitions, these happen during asset loading in mission and agents interaction during missions." & vbNewLine & vbNewLine &
-                                            "If you're experiencing slowdown during combat or mission, you can disable this behaviour by changing CatchNative2Managed to false in config.json" &
-                                            IIf(IsMcmLoaded(), " or from Mod Options > Better Exception Window > (uncheck) Enable Native to Managed exception handler", "") & vbNewLine & vbNewLine &
-                                            "Do you want to keep this behaviour enabled? (Restart required if you pick 'Disable it')",
-                                            Sub()
-                                                CatchNative2Managed = True
-                                                IsFirstTime = False
-                                                SaveSettings()
-                                            End Sub,
-                                            Sub()
-                                                CatchNative2Managed = False
-                                                IsFirstTime = False
-                                                SaveSettings()
-                                            End Sub,
-                                            "Keep it enabled", "Disable it")
-                        End Sub)
-                    End If
                     If Not CheckIsAssemblyLoaded("Bannerlord.ButterLib.dll") Then
                         Exit Sub
                     End If
@@ -96,7 +74,6 @@ Namespace Global.BetterExceptionWindow
             ReadConfig()
             If EnableStdoutConsole Then SpawnConsole() Else StartLogger()
             DownloadExceptionDefinitions()
-            InitPatch()
             If IsDebuggedByDnspy Then
             Else
                 If AllowInDebugger Then InitPatch()
